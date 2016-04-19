85f5f3cfa0b0fade48f1429fce8fcaa5f261f08e
EmbeddedPi <dawishart@yahoo.co.uk>
2016-04-19 17:08:21 +0100

    Removed bukkit related code not related for this part.

--- src/udpServerLED/Main.java
+++ src/udpServerLED/Main.java
@@ -1,21 +1,14 @@
 package udpServerLED;
 
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.event.Listener;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
-public final class Main extends JavaPlugin implements Listener {
-	
-	private short local = 0;
-	private short notLocal = 0;
-	private boolean areYouLocal = false;
-	private boolean recentJoin = false;
-	private String recentPlayer = "";
-	private String recentPlayerIP = "";
+public class Main {
+		
 	private static final String gpioPath="/sys/class/gpio";
 	private static final String exportPath= gpioPath + "/export";
 	private static final String unexportPath= gpioPath + "/unexport";
@@ -26,82 +19,58 @@ public final class Main extends JavaPlugin implements Listener {
 	private static final String gpioOn = "1";
 	private static final String gpioOff = "0";
 	private static final int[] gpioChannel = {18,23,24};
-		
-	@Override
-    public void onEnable() {
-		// register listener
-		getServer().getPluginManager().registerEvents(this, this);
-				
+
+public Main () throws IOException {		
 		// Open file handles for GPIO unexport and export
-		try {
-			FileWriter unexportFile = new FileWriter(unexportPath);
-			FileWriter exportFile = new FileWriter(exportPath);
-		
-			// Initialise GPIO settings
-			for (Integer channel : gpioChannel) {
-				File exportFileCheck = new File(getDevicePath(channel));
-				if (exportFileCheck.exists()) {
-					unexportFile.write(channel.toString());
-					unexportFile.flush();	
+				try {
+					FileWriter unexportFile = new FileWriter(unexportPath);
+					FileWriter exportFile = new FileWriter(exportPath);
+				
+					// Initialise GPIO settings
+					for (Integer channel : gpioChannel) {
+						File exportFileCheck = new File(getDevicePath(channel));
+						if (exportFileCheck.exists()) {
+							unexportFile.write(channel.toString());
+							unexportFile.flush();	
+							}
+						// Set port for use
+						exportFile.write(channel.toString());
+						exportFile.flush();
+						//Set direction file
+						FileWriter directionFile = new FileWriter(getDirectionPath(channel));
+						directionFile.write(gpioOut);
+						directionFile.flush();
+						directionFile.close();
+						}
+							
+					unexportFile.close();
+					exportFile.close();
 					}
-				// Set port for use
-				exportFile.write(channel.toString());
-				exportFile.flush();
-				//Set direction file
-				FileWriter directionFile = new FileWriter(getDirectionPath(channel));
-				directionFile.write(gpioOut);
-				directionFile.flush();
-				directionFile.close();
-				}
-					
-			unexportFile.close();
-			exportFile.close();
-			}
-		catch (Exception exception) {
-			exception.printStackTrace();
-			}
-		
-		// Switch on server LED
-		writeLED (gpioChannel[0], gpioOn);
-		getLogger().info("piLED is switched on."); 
+				catch (Exception exception) {
+					exception.printStackTrace();
+					}
+				
+	DatagramSocket serverSocket = new DatagramSocket(9876);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+//TODO try catch this block and close socket/throw exception
+        while(true){
+                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                 serverSocket.receive(receivePacket);
+                 String sentence = new String( receivePacket.getData());
+                 System.out.println("RECEIVED: " + sentence);
+                 InetAddress IPAddress = receivePacket.getAddress();
+                 int port = receivePacket.getPort();
+				   System.out.println("Got this from " + IPAddress + " @ port " + port);
+                 String capitalizedSentence = sentence.toUpperCase();
+                 sendData = capitalizedSentence.getBytes();
+                 DatagramPacket sendPacket =
+                 new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                 serverSocket.send(sendPacket);
+                 updateLED();
+              }
+        
 	}
- 
-    @Override
-    public void onDisable() {
-        //Switch off all LEDs
-        writeLED (gpioChannel[0], gpioOff);
-        writeLED (gpioChannel[1], gpioOff);
-        writeLED (gpioChannel[2], gpioOff);
-        getLogger().info("piLED has been extinguished.");
-    }
-    
-    // Someone joins server
-    @EventHandler
-    public void onLogin(PlayerJoinEvent event) {
-    	// Check whether internal or external IP address
-    	recentPlayer = event.getPlayer().getName();
-    	recentPlayerIP = event.getPlayer().getAddress().getHostString();
-    	recentJoin = true;
-    	isLocal();
-    	// Update local/notLocal LED status according
-    	updateLED();
-        // The following lines are for test purposes only
-    	debugMessage();	
-    }
-    
-    // Someone leaves server
-    @EventHandler
-    public void onLogout(PlayerQuitEvent event) {
-    	// Check whether internal or external IP address
-    	recentPlayer = event.getPlayer().getName();
-    	recentPlayerIP = event.getPlayer().getAddress().getHostString();
-    	recentJoin = false;
-    	isLocal();
-    	// Update local/notLocal LED status according
-    	updateLED();
-    	// The following lines are for test purposes only
-    	debugMessage();
-    }
 
     // Variable setting for device path
     private static String getDevicePath(int pinNumber) {
@@ -117,53 +86,13 @@ public final class Main extends JavaPlugin implements Listener {
     private static String getValuePath(int pinNumber) {
  	   return String.format(valuePath, pinNumber);
     }
-    
-    // Determine player location
-    private void isLocal() {
-    	// Set local variables and count
-    	if (recentPlayerIP.equals("192.168.1.1")) {
-    		areYouLocal = false;
-    		if (recentJoin) {
-    			notLocal++;
-    			}
-    			else {
-    			notLocal--;
-    			}
-    		} 	
-    	else if (recentPlayerIP.startsWith("192.168.1")) {
-    		areYouLocal = true;
-    		if (recentJoin) {
-    			local++;
-    			}
-    		else {
-    		local--;
-    			}
-    		}
-    	else {
-    		areYouLocal = false;
-    		if (recentJoin) {
-    			notLocal++;
-    			}
-    			else {
-    			notLocal--;
-    			}
-    		}
-    	}
 
     // Update player LED status
     private void updateLED() {
-    	if (local > 0) {
     		writeLED (gpioChannel[1], gpioOn);
-    		}
-    	else {
     		writeLED (gpioChannel[1], gpioOff);
-    		}
-    	if (notLocal > 0) {
     		writeLED (gpioChannel[2], gpioOn); 		
-    		}
-    	else {
     		writeLED (gpioChannel[2], gpioOff);    			
-    		}	
     }
     
     // LED IO 
@@ -177,22 +106,6 @@ public final class Main extends JavaPlugin implements Listener {
         catch (Exception exception) {
         	exception.printStackTrace();
         }
-    }
-    
-    // Test messages to server log
-    private void debugMessage() {
-    	String joinString ="";
-    	if (recentJoin) {
-    		joinString =" has joined Olly's server.";
-    		}
-    	else {
-    	joinString =" has left Olly's server.";
-    		}
-    	getLogger().info(recentPlayer + joinString); 
-    	getLogger().info("They live at " + recentPlayerIP);  	
-    	getLogger().info("Are they local is " + areYouLocal);
-    	getLogger().info("Locals online = " + local);
-    	getLogger().info("Not locals online = " + notLocal);   	
     }
 }
 
